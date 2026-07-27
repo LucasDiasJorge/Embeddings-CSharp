0e8087798dc9658853e9ecd3e403d739fb15d58e
Lucas Dias Jorge <lucas_jorg@hotmail.com>
2026-07-26 21:18:49 -0300

    Adiciona exemplo com Semantic Kernel e Ollama
    Foram adicionados três arquivos: `.gitignore` (configurado para ignorar arquivos/pastas comuns do .NET/VS e temporários de SO), `Program.cs` (exemplo de geração de embeddings de texto usando Microsoft Semantic Kernel com Ollama) e `project.csproj` (aplicação console .NET 10.0 com dependência do pacote `Microsoft.SemanticKernel.Connectors.Ollama`).

---
+++ project/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.SemanticKernel.Embeddings;
+using OllamaSharp;
+
+namespace project;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using OllamaApiClient ollamaApiClient = new OllamaApiClient(
+            uriString: "http://localhost:11434",
+            defaultModel: "nomic-embed-text");
+
+        ITextEmbeddingGenerationService service = ollamaApiClient.AsTextEmbeddingGenerationService();
+        IList<ReadOnlyMemory<float>> embeddings = await service.GenerateEmbeddingsAsync([
+            "Café Brasileiro",
+            "Café Italiano",
+            "Café Português"]);
+
+        foreach (ReadOnlyMemory<float> embedding in embeddings)
+            Console.WriteLine(embedding);
+
+    }
+}
